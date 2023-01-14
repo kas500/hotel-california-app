87dec359c00959440fe453bfddb6d89153d3c8a3
enanesh <edna.ganesha.gr@gmail.com>
2023-01-13 19:43:49 -0800

    Creates the class rooms and reservations

---
+++ models/reservations.js
@@ -0,0 +1,30 @@
+const { Model, DataTypes } = require('sequelize');
+// const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Reservations extends Model { }
+
+Reservations.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'reservations',
+    }
+);
+
+module.exports = Reservations;
