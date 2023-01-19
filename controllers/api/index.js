392d9cc2e8507739cf8d705b259f44c7bf4c9763
enanesh <edna.ganesha.gr@gmail.com>
2023-01-19 01:21:59 -0800

    Changes model Comments let out the time stamp false statement, so the table comments can have a 'created_at' and 'updated_at

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const reservationRoutes = require('./reservationRoutes');
+// const commentRoutes = require('./commentRoutes');
+
 
 
 router.use('/users', userRoutes);
 router.use('/reservation', reservationRoutes);
+// router.use('/comment', commentRoutes);
 
 module.exports = router;
