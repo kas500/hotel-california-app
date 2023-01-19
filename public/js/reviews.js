3cb23d249987d7486b64c2dd8291cf4ef022c529
enanesh <edna.ganesha.gr@gmail.com>
2023-01-19 11:32:27 -0800

    Sets up Route to create a comment, working properly

---
+++ public/js/reviews.js
@@ -0,0 +1,28 @@
+const reviewsFormHandler = async (event) => {
+    event.preventDefault();
+
+   console.log(" **********reviews.js")
+    const reviews = document.querySelector('#reviews').value.trim();
+   
+
+
+    if (reviews) {
+
+
+        const response = await fetch('/api/reviews', {
+            method: 'POST',
+            body: JSON.stringify({ comment: reviews}),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            alert('Thanks for your review!');
+            document.location.replace('/');
+        } else {
+            console.log(response);
+            alert('Failed to create review.')
+        }
+    }
+};
+
+document.querySelector('#reviews-form').addEventListener('submit', reviewsFormHandler);
