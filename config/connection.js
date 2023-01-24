6339da4138cf3cd166f7c9079c03becb5f03d28b
Anton Krasnikov <krasnikovanton84@gmail.com>
2023-01-23 19:42:49 -0800

    Adds JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,10 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
+let sequelize;
 
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL)
+} else {
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -10,6 +14,6 @@ const sequelize = new Sequelize(
     dialect: 'mysql',
     port: 3306,
   }
-);
+)};
 
 module.exports = sequelize;
